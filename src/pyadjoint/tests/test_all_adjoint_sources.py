ca9104920e6865cd870bcc8779f9bd76e3677898
chukren <youyir@princeton.edu>
2016-10-27 12:02:12 -0400

    Update adjoint source test module for new Config Class

--- src/pyadjoint/tests/test_all_adjoint_sources.py
+++ src/pyadjoint/tests/test_all_adjoint_sources.py
@@ -29,18 +29,6 @@ def test_normal_adjoint_source_calculation(adj_src):
     """
     Make sure everything at least runs. Executed for every adjoint source type.
     """
-    config = pyadjoint.Config(min_period=30.0, max_period=75.0,
-                              lnpt=15,
-                              transfunc_waterlevel=1.0E-10,
-                              water_threshold=0.02,
-                              ipower_costaper=10,
-                              min_cycle_in_window=3,
-                              taper_percentage=0.3,
-                              taper_type='hann',
-                              mt_nw=4,
-                              phase_step=1.5,
-                              use_cc_error=False,
-                              use_mt_error=False)
 
     obs, syn = pyadjoint.utils.get_example_data()
     obs = obs.select(component="Z")[0]
@@ -49,10 +37,50 @@ def test_normal_adjoint_source_calculation(adj_src):
 
     window = [[2076., 2418.0]]
 
-    a_src = pyadjoint.calculate_adjoint_source(
-        adj_src_type=adj_src, observed=obs,
-        synthetic=syn, config=config, window=window,
-        adjoint_src=True, plot=False)
+    if adj_src == "waveform_misfit":
+        config = pyadjoint.ConfigWaveForm(min_period=30.0,
+                                          max_period=75.0,
+                                          taper_type='hann',
+                                          taper_percentage=0.3)
+
+    if adj_src == "cc_traveltime_misfit":
+        config = pyadjoint.ConfigCrossCorrelation(min_period=30.0,
+                                                  max_period=75.0,
+                                                  taper_type='hann',
+                                                  taper_percentage=0.3,
+                                                  measure_type='dt',
+                                                  dt_sigma_min=1.0,
+                                                  dlna_sigma_min=0.5)
+
+    if adj_src == "multitaper_misfit":
+        config = pyadjoint.ConfigMultiTaper(min_period=30.0,
+                                            max_period=75.0,
+                                            lnpt=15,
+                                            transfunc_waterlevel=1.0E-10,
+                                            water_threshold=0.02,
+                                            ipower_costaper=10,
+                                            min_cycle_in_window=3,
+                                            taper_percentage=0.3,
+                                            taper_type='hann',
+                                            mt_nw=4,
+                                            num_taper=5,
+                                            dt_fac=2.0,
+                                            phase_step=1.5,
+                                            err_fac=2.5,
+                                            dt_max_scale=3.5,
+                                            measure_type='dt',
+                                            dt_sigma_min=1.0,
+                                            dlna_sigma_min=0.5,
+                                            use_cc_error=False,
+                                            use_mt_error=False)
+
+    a_src = pyadjoint.calculate_adjoint_source(adj_src_type=adj_src,
+                                               observed=obs,
+                                               synthetic=syn,
+                                               config=config,
+                                               window=window,
+                                               adjoint_src=True,
+                                               plot=False)
 
     # a_src = pyadjoint.calculate_adjoint_source(
     #    adj_src, obs, syn, 20, 100, start, end)
@@ -67,23 +95,47 @@ def test_no_adjoint_src_calculation_is_honored(adj_src):
     """
     If no adjoint source is requested, it should not be returned/calculated.
     """
-    config = pyadjoint.Config(min_period=30.0, max_period=75.0,
-                              lnpt=15,
-                              transfunc_waterlevel=1.0E-10,
-                              water_threshold=0.02,
-                              ipower_costaper=10,
-                              min_cycle_in_window=3,
-                              taper_percentage=0.3,
-                              taper_type='hann',
-                              mt_nw=4,
-                              phase_step=1.5,
-                              use_cc_error=False,
-                              use_mt_error=False)
-
     obs, syn = pyadjoint.utils.get_example_data()
     obs = obs.select(component="Z")[0]
     syn = syn.select(component="Z")[0]
     window = [[2076., 2418.0]]
+
+    if adj_src == "waveform_misfit":
+        config = pyadjoint.ConfigWaveForm(min_period=30.0,
+                                          max_period=75.0,
+                                          taper_type='hann',
+                                          taper_percentage=0.3)
+
+    if adj_src == "cc_traveltime_misfit":
+        config = pyadjoint.ConfigCrossCorrelation(min_period=30.0,
+                                                  max_period=75.0,
+                                                  taper_type='hann',
+                                                  taper_percentage=0.3,
+                                                  measure_type='dt',
+                                                  dt_sigma_min=1.0,
+                                                  dlna_sigma_min=0.5)
+
+    if adj_src == "multitaper_misfit":
+        config = pyadjoint.ConfigMultiTaper(min_period=30.0,
+                                            max_period=75.0,
+                                            lnpt=15,
+                                            transfunc_waterlevel=1.0E-10,
+                                            water_threshold=0.02,
+                                            ipower_costaper=10,
+                                            min_cycle_in_window=3,
+                                            taper_percentage=0.3,
+                                            taper_type='hann',
+                                            mt_nw=4,
+                                            num_taper=5,
+                                            dt_fac=2.0,
+                                            phase_step=1.5,
+                                            err_fac=2.5,
+                                            dt_max_scale=3.5,
+                                            measure_type='dt',
+                                            dt_sigma_min=1.0,
+                                            dlna_sigma_min=0.5,
+                                            use_cc_error=False,
+                                            use_mt_error=False)
 
     a_src = pyadjoint.calculate_adjoint_source(
         adj_src_type=adj_src, observed=obs,
